5aa8b769b1cf136eb4cdd429d7f9e9e7d19a8f79
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2023-01-08 20:47:21 +0900

    api 테스트

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import axios from "axios";
+
 function App() {
+  const testAxios = async () => {
+    try {
+      const res = await axios.get("https://www.gscook-back.link/notice/list");
+
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  testAxios();
+
   return <h1>AWS Test</h1>;
 }
 
